cfbd6424bdbf320294912a7abf27de647fecbfb1
Ahmad Adham <ahmadadham09work@hotmail.com>
2025-11-12 01:20:25 +0200

    modified:   Library Files/MyTFT.cpp modified:   Library Files/MyTFT.h

--- Library Files/MyTFT.cpp
+++ Library Files/MyTFT.cpp
@@ -2,15 +2,6 @@
 #include <Arduino.h>
 #include <string.h>
 
-// Color definitions
-#define BLACK   0x0000
-#define BLUE    0x001F
-#define RED     0xF800
-#define GREEN   0x07E0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
 
 // ------------------ Constructor ------------------
 MyTFT::MyTFT(uint16_t id, uint8_t rotation) : _id(id), _rotation(rotation) {}
--- Library Files/MyTFT.h
+++ Library Files/MyTFT.h
@@ -4,6 +4,16 @@
 #include <MCUFRIEND_kbv.h>
 #include <Adafruit_GFX.h>
 
+// Color definitions
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
 class MyTFT {
   public:
     MyTFT(uint16_t id = 0x9341, uint8_t rotation = 0);
